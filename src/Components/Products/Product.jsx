17fd3fae23f8c52f666c64c2b9122840c949bfff
Yogesh Negi <113903081+itsyogeshnegi@users.noreply.github.com>
2023-08-03 21:21:23 +0530

    design setting

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -3,7 +3,7 @@ import "./Product.css";
 const Product = () => {
   const [Data, setData] = useState([]);
 
-  let apikey = "https://fakestoreapi.com/products?limit=15";
+  let apikey = "https://fakestoreapi.com/products?limit=10";
 
 
   function fetchData(url){
@@ -18,32 +18,35 @@ const Product = () => {
   fetchData(apikey);
 
   return (
-  
-  <div className="productsShow"> 
+    <>
+    <div className="productHeading">
+      <h1>OUR PRODUCTS</h1>
+    </div>
+      <div className="productsShow">
+        {Data.map(i => {
+          const { id, category, image, price, rating, title } = i;
 
-    {
-      Data.map(i =>{
-        const {id , category , image , price, rating, title} = i;
-
-        return(
-          <div className="box" key={id}>
-            <div className="image-box">
-              <img src={image}/>
+          return (
+            <div className="box" key={id}>
+              <div className="image-box">
+                <img src={image} />
+              </div>
+              <div className="text-box">
+                <h3>{category}</h3>
+                <p>{title}</p>
+                <p>${price}</p>
+                <p>Rating:{rating.rate}</p>
+              </div>
+              <button className="addCart">
+                Add to Cart
+                <i className="fa-solid fa-cart-shopping" />
+              </button>
             </div>
-            <div className="text-box">
-              <h3>{category}</h3>
-              <p>{title}</p>
-              <p>${price}</p>
-              <p>Rating:{rating.rate}</p>
-            </div>
-            
-          </div>
-        )
-
-      })
-    }
-  </div> 
-  )
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Product;
